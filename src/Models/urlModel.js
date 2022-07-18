1f9a8378a4d371f1686fd3b2f1530133bbf9a896
mn-pandey <aman7607315523@gmail.com>
2022-07-18 11:43:25 +0530

    begins

---
+++ src/Models/urlModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+// instantiate a mongoose schema
+const URLSchema = new mongoose.Schema({
+    urlCode: String,
+    longUrl: String,
+    shortUrl: String
+})
+
+// create a model from schema and export it
+module.exports = mongoose.model('Url', URLSchema)
---
+++ src/routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const urlController = require("../Controllers/urlController");
+
+router.post("/url/shorten",urlController.)
+
+module.exports = router;
