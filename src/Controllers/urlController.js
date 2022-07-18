5e4e32cd98707ab2d4758db8c948b57190ee8380
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-07-18 16:36:07 +0530

    url Validation

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,31 +1,29 @@
 const shortid = require('shortid')
 const Url = require('../Models/urlModel')
+const validator = require('validator')
 
 const baseUrl = 'http://localhost:3000'
-const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
-
 
 const shortenUrl = async function (req, res) {
     try {
-        const { longUrl } = req.body // destructure the longUrl from req.body.longUrl
-
         if (Object.keys(req.body).length == 0) {
             return res.status(400).send({ status: false, msg: "Body should not be Empty.. " })
         }
+        const { longUrl } = req.body
+        if(!longUrl)    return res.status(400).send({ status: false, msg: "Please provide longUrl" })
 
         const urlCode = shortid.generate().toLowerCase()
 
         // const check = await Url.findOne({urlCode})
         // if(check)   urlCode = shortid.generate().toLowerCase()
 
-        if (urlRegex.test(longUrl)) {
-
+        if (validator.isURL(longUrl)) {
             let url = await Url.findOne({longUrl}).select({ _id: 0, __v: 0 })
 
             if (url) {
                 res.status(200).send({ status: true, data: url })
             } else {
-                // join the generated short code the the base url
+                // join the generated short code and the base url
                 const shortUrl = baseUrl + '/' + urlCode
 
                 let url = await Url.create({longUrl, shortUrl, urlCode})
@@ -39,7 +37,7 @@ const shortenUrl = async function (req, res) {
             }
         }
         else {
-            res.status(400).send({ status: false, message: "invalid longUrl" })
+            res.status(400).send({ status: false, message: "Invalid longUrl" })
         }
     }
     catch (err) {
@@ -56,15 +54,12 @@ const getUrlCode = async function (req, res) {
             urlCode: req.params.urlCode
         })
         if (url) {
-
             return res.status(302).send({ status: true, data: url.longUrl })
         } else {
-            // else return a not found 404 status
             return res.status(404).send({ status: false, message: 'No URL Found' })
         }
 
     }
-    // exception handler
     catch (err) {
         console.error(err)
         res.status(500).send({ status: false, message: err.message })
