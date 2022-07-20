bdbd3b97b4959132b52ce26526d21750c1be7972
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-07-20 16:48:16 +0530

    Caching

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,8 +1,27 @@
 const shortid = require('shortid')
 const Url = require('../Models/urlModel')
 const validator = require('validator')
+const { promisify } = require("util");
+const redis = require('redis')
+
 
 const baseUrl = 'http://localhost:3000'
+
+const redisClient = redis.createClient(
+    12520,
+    "redis-12520.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("AP8turZ2WJZedPztV39T3jDOIJEpAWwf", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const shortenUrl = async function (req, res) {
     try {
@@ -12,15 +31,18 @@ const shortenUrl = async function (req, res) {
         const { longUrl } = req.body
         if(!longUrl)    return res.status(400).send({ status: false, msg: "Please provide longUrl" })
 
+        if(typeof longUrl != "string")  return res.status(400).send({ status: false, msg: "Invalid longUrl format" })
         const urlCode = shortid.generate().toLowerCase()
 
-        // const check = await Url.findOne({urlCode})
-        // if(check)   urlCode = shortid.generate().toLowerCase()
-
         if (validator.isURL(longUrl)) {
+            let cachedUrl = await GET_ASYNC(`${longUrl}`)
+            cachedUrl = JSON.parse(cachedUrl)
+            if(cachedUrl)    return res.status(200).send({ status: true, data: cachedUrl})
+
             let url = await Url.findOne({longUrl}).select({ _id: 0, __v: 0 })
 
             if (url) {
+                await SET_ASYNC(`${longUrl}`, JSON.stringify(url))
                 res.status(200).send({ status: true, data: url })
             } else {
                 // join the generated short code and the base url
@@ -50,10 +72,13 @@ const shortenUrl = async function (req, res) {
 const getUrlCode = async function (req, res) {
     try {
         // find a document match to the code in req.params.code
+        let cachedUrl = await GET_ASYNC(`${req.params.urlCode}`)
+        if(cachedUrl)    return res.status(302).redirect(cachedUrl)
         const url = await Url.findOne({
             urlCode: req.params.urlCode
         })
         if (url) {
+            await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(url.longUrl))
             return res.status(302).redirect(url.longUrl)
         } else {
             return res.status(404).send({ status: false, message: 'No URL Found' })
